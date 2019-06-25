0f23aeb5de137cc08c36b1a47fa5c71a2b79be80
Marcel Heil <marcel.heil@valtech.de>
2019-06-25 11:15:19 +0200

    * add room prices and comments

--- sql/schema.sql
+++ sql/schema.sql
@@ -13,11 +13,12 @@ roomCode VARCHAR(3) NOT NULL,
 FOREIGN KEY (roomCode) REFERENCES RoomType (roomCode)
 )
 
-
+-- This could also be a transaction table as the prices may vary with the season and occupancy.
+-- Keeping it simple for this exercise with fixed prices.
 CREATE TABLE RoomPrice (
 roomId INTEGER NOT NULL,
 price DOUBLE NOT NULL,
-currencyCode VARCHAR(3),
+currency VARCHAR(3),
 FOREIGN KEY (roomId) REFERENCES Rooms (id)
 )
 
@@ -72,6 +73,29 @@ INSERT INTO Rooms (roomCode) VALUES ('JST')
 INSERT INTO Rooms (roomCode) VALUES ('APT')
 INSERT INTO Rooms (roomCode) VALUES ('APT')
 
+-- Add room prices
+-- Double Standard
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (1, 99.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (2, 99.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (3, 109.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (4, 109.0, 'EUR')
+-- Double Superior
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (5, 119.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (6, 119.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (7, 139.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (8, 139.0, 'EUR')
+-- Family Small
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (9, 149.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (10, 149.0, 'EUR')
+-- Family Large
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (11, 169.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (12, 169.0, 'EUR')
+-- Junior Suites
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (13, 159.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (14, 159.0, 'EUR')
+-- Apartments
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (15, 199.0, 'EUR')
+INSERT INTO RoomPrice (roomId, price, currency) VALUES (16, 199.0, 'EUR')
 
 
 
